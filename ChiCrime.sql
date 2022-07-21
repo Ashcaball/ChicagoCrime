560869781063e3745b016374a4b3d7d86e9a87ab
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-07-21 19:40:14 -0400

    Update ChiCrime.sql

--- ChiCrime.sql
+++ ChiCrime.sql
@@ -1,13 +1,46 @@
 /* What blocks experience the most crime */
-/* What kind of crime is experience the most?*/
 
-SELECT count(primary_type) AS Robbery
+SELECT block, count(block) AS HighCrimeBlock
 FROM `bigquery-public-data.chicago_crime.crime` 
-WHERE primary_type = 'ROBBERY'
+Group by block
+order by HighCrimeBlock desc
 
- -- result: 284935 out of 7585046 
- -- Robbery makes up 3.76% 
+ -- result: 100XX W OHARE ST 0.21%, 001XX N STATE ST 0.19%, 076XX S CICERO AVE 0.13% out of 62252 blocks that exisy in Chicago
 
-/* What dates is crime highest at?*/
+/* Top 3 crimes commited?*/
+
+SELECT primary_type, count(primary_type) AS AmountCommited
+FROM `bigquery-public-data.chicago_crime.crime` 
+Group by primary_type 
+order by AmountCommited desc
+
+ -- result: THEFT 21.07%, BATTERY 18.34%, CRIMINAL DAMAGE 11.4%
+
 /* What percentage of crimes result in arrest? */
-/* What locations do most crimes occur at */
+
+SELECT count(arrest) AS Arrested
+FROM `bigquery-public-data.chicago_crime.crime` 
+WHERE arrest = false 
+
+ -- Result: 26.52%
+
+/* What locations do most crimes occur at? (ex: sidewalk, alley, school) */
+
+SELECT location_description, count(location_description) AS location_of_crime
+FROM `bigquery-public-data.chicago_crime.crime` 
+Group by location_description
+
+ -- Result: Residence 16.89% 
+ 
+ /* Which years had highest and lowest amount of crimes commited?*/
+ 
+ SELECT year, count(year) AS CrimeYear
+FROM `bigquery-public-data.chicago_crime.crime` 
+Group BY year
+order by CrimeYear desc
+
+ -- result: HIGHh: 2002 with 486788 crimes commited, LOW: 2021 with 207594 crimes commited
+ 
+ 
+ 
+ 
